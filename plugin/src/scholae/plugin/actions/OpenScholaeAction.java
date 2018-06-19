0c7c618a7ffa04027f1a2cab1c85cbd01f86fce7
nsis <grand-roman@yandex.ru>
2018-06-19 11:40:19 +0700

    Issue #2 Create an entry point for the plugin

---
+++ plugin/src/scholae/plugin/actions/OpenScholaeAction.java
@@ -0,0 +1,12 @@
+package scholae.plugin.actions;
+
+
+import com.intellij.openapi.actionSystem.*;
+
+public class OpenScholaeAction extends AnAction{
+
+        @Override
+        public void actionPerformed(AnActionEvent anActionEvent) {
+            System.out.println("Hello!");
+        }
+    }
