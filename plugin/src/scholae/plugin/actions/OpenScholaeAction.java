c616ccc5787649293af7155cda4f9cbf1b65c9a4
Nikolay Grebenshikov <grebenshikov.n@gmail.com>
2018-06-19 12:05:42 +0700

    Make the action enabled

--- plugin/src/scholae/plugin/actions/OpenScholaeAction.java
+++ plugin/src/scholae/plugin/actions/OpenScholaeAction.java
@@ -5,8 +5,14 @@ import com.intellij.openapi.actionSystem.*;
 
 public class OpenScholaeAction extends AnAction{
 
-        @Override
-        public void actionPerformed(AnActionEvent anActionEvent) {
-            System.out.println("Hello!");
-        }
+    @Override
+    public void update(AnActionEvent e) {
+        super.update(e);
+        e.getPresentation().setEnabled(true);
     }
+
+    @Override
+    public void actionPerformed(AnActionEvent anActionEvent) {
+        System.out.println("Hello!");
+    }
+}
