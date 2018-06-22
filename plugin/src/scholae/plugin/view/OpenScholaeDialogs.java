aa4d96f4ce29748f7ee1d5f0554092181bc17aea
nsis <grand-roman@yandex.ru>
2018-06-22 14:50:13 +0700

    Issue #2 Create an entry point for the plugin

--- plugin/src/scholae/plugin/view/OpenScholaeDialogs.java
+++ plugin/src/scholae/plugin/view/OpenScholaeDialogs.java
@@ -1,28 +1,91 @@
 package scholae.plugin.view;
 
 import javax.swing.*;
+import javax.swing.event.*;
+import java.awt.*;
+import java.awt.image.VolatileImage;
+import java.util.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class OpenScholaeDialogs extends JFrame {
     public JPanel Gotpanel;
-    private JButton button1;
-    private JSlider slider1;
-    private JSlider slider2;
-    private JCheckBox checkBox1;
-    private JList list1;
+    public JButton button1;
+    public JSlider slider1;
+    public JSlider slider2;
+    public JCheckBox checkBox1;
+    public JList list1;
+    public String str_slider1;
+    public String str_slider2;
 
     public OpenScholaeDialogs() {
         setContentPane(Gotpanel);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        pack();
-        setVisible(true);
+
         button1.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
-                JOptionPane.showMessageDialog(null, "Hello");
-
+                JOptionPane.showMessageDialog(null, str_slider1);
             }
         });
+
+        /*slider1 = new JSlider( 1, 5, 5);
+        slider1.setFont(new Font("Dialog", Font.BOLD, 10));
+        slider1.setMajorTickSpacing(4);
+        slider1.setMajorTickSpacing(2);
+        slider1.setPaintTicks(true);
+        slider1.setPaintLabels(true);
+        slider1.setSnapToTicks(true);*/
+        // slider1.getValue();
+        /*slider1.addChangeListener(new ChangeListener() {
+            @Override
+            public void stateChanged(ChangeEvent e) {
+                // String str = null;
+                *//*System.out.println(slider1.getValue());
+                if (event.getSource() == slider1) {
+                    //Получаем установленное значение;
+                    str = (new Integer(slider1.getValue())).toString();
+
+                }*//*
+
+                JSlider source = (JSlider)e.getSource();
+                if(!source.getValueIsAdjusting())
+                {
+                    //textField.setText(String.valueOf(source.getValue()));
+                    int gain = source.getValue();
+                    System.out.println(gain);
+                    //System.out.println("***** GAIN ***** "+gain);
+                    // GetGain g = new GetGain(gain);
+                } else {
+                    System.out.println("wrf");
+                }
+            }
+        });*/
+        //getContentPane().add(slider1);
+        /*
+        Hashtable<Integer, Component> labelTable = new Hashtable<Integer, Component>();
+        labelTable.put(1, new JLabel("A"));
+        labelTable.put(5, new JLabel("B"));
+        slider1.setLabelTable(labelTable);
+        //addSlider(slider1, "Custom labels");*/
+
+        slider1.addChangeListener(new ChangeListener() {
+            @Override
+            public void stateChanged(ChangeEvent changeEvent) {
+                JSlider source = (JSlider) changeEvent.getSource();
+                str_slider1 = "" + source.getValue();
+            }
+        });
+
+
+        slider2.addChangeListener(new ChangeListener() {
+            @Override
+            public void stateChanged(ChangeEvent changeEvent) {
+                JSlider source = (JSlider) changeEvent.getSource();
+                str_slider2 = "" + source.getValue();
+            }
+        });
+        pack();
+        setVisible(true);
     }
 }
